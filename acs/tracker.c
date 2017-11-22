a7e875d11cbb769dc8ee240614ec5b28ebc751e5
alutman <alexander.lutman@gmail.com>
2017-11-23 00:36:46 +1100

    Ensure only one script is running

--- acs/tracker.c
+++ acs/tracker.c
@@ -58,71 +58,77 @@ function int timeToStr(int time) {
     return StrParam(s:pad(mins), s:":", s:pad(secs));
   }
   return 0;
-  
 }
 
-script 400 ENTER clientside
-{
+function void displayStats(void) {
   int tagColor = "g";
   int dataColor = "d";
   int completeColor = "n";
-  while(true) {
-    int countColor = 0;
-    int fullMessage = "";
-    if(getCVar(SHOW_SECRET_CVAR)) {
-      int ts = GetLevelInfo(LEVELINFO_TOTAL_SECRETS);
-      int fs = GetLevelInfo(LEVELINFO_FOUND_SECRETS);
-      countColor = dataColor;
-      if(GetCVar(COLOR_COUNTS_CVAR) && fs >= ts) {
-        countColor = completeColor;
-      }
-      int secretMsg = StrParam(s:"\c", s:tagColor, s:"s: \t\t", s:"\c", s:countColor, d:fs, s:"/", d:ts);
-      fullMessage = StrParam(s:fullMessage, s:secretMsg, s:"\n");
+  int countColor = 0;
+  int fullMessage = "";
+  if(getCVar(SHOW_SECRET_CVAR)) {
+    int ts = GetLevelInfo(LEVELINFO_TOTAL_SECRETS);
+    int fs = GetLevelInfo(LEVELINFO_FOUND_SECRETS);
+    countColor = dataColor;
+    if(GetCVar(COLOR_COUNTS_CVAR) && fs >= ts) {
+      countColor = completeColor;
     }
-    if(getCVar(SHOW_ITEM_CVAR)) {
-      int ti = GetLevelInfo(LEVELINFO_TOTAL_ITEMS);
-      int fi = GetLevelInfo(LEVELINFO_FOUND_ITEMS);
-      countColor = dataColor;
-      if(GetCVar(COLOR_COUNTS_CVAR) && fi >= ti) {
-        countColor = completeColor;
-      }
-      fullMessage = StrParam(s:fullMessage, s:"\c", s:tagColor, s:"i: \t\t\t", s:"\c", s:countColor, d:fi, s:"/", d:ti, s:"\n");
+    int secretMsg = StrParam(s:"\c", s:tagColor, s:"s: \t\t", s:"\c", s:countColor, d:fs, s:"/", d:ts);
+    fullMessage = StrParam(s:fullMessage, s:secretMsg, s:"\n");
+  }
+  if(getCVar(SHOW_ITEM_CVAR)) {
+    int ti = GetLevelInfo(LEVELINFO_TOTAL_ITEMS);
+    int fi = GetLevelInfo(LEVELINFO_FOUND_ITEMS);
+    countColor = dataColor;
+    if(GetCVar(COLOR_COUNTS_CVAR) && fi >= ti) {
+      countColor = completeColor;
     }
-    if(getCVar(SHOW_MONSTER_CVAR)) {
-      int tm = GetLevelInfo(LEVELINFO_TOTAL_MONSTERS);
-      int km = GetLevelInfo(LEVELINFO_KILLED_MONSTERS);
-      countColor = dataColor;
-      if(GetCVar(COLOR_COUNTS_CVAR) && km >= tm) {
-        countColor = completeColor;
-      }
-      fullMessage = StrParam(s:fullMessage, s:"\c", s:tagColor, s:"k: \t\t", s:"\c", s:countColor, d:km, s:"/", d:tm, s:"\n");
+    fullMessage = StrParam(s:fullMessage, s:"\c", s:tagColor, s:"i: \t\t\t", s:"\c", s:countColor, d:fi, s:"/", d:ti, s:"\n");
+  }
+  if(getCVar(SHOW_MONSTER_CVAR)) {
+    int tm = GetLevelInfo(LEVELINFO_TOTAL_MONSTERS);
+    int km = GetLevelInfo(LEVELINFO_KILLED_MONSTERS);
+    countColor = dataColor;
+    if(GetCVar(COLOR_COUNTS_CVAR) && km >= tm) {
+      countColor = completeColor;
     }
-    if(getCVar(SHOW_TIME_CVAR)) {
-      int time = Timer()/35;
-      int par = GetLevelInfo(LEVELINFO_PAR_TIME);
-      int timeTag = "t: ";
-      int timeStr = StrParam(s:timeToStr(time));
-      int parStr = StrParam(s:timeToStr(par));
-      int col = dataColor;
-      if(getCVar(COLOR_TIME_CVAR)) {
-        col = timeColor(time, par);  
-      }
-
-      fullMessage = StrParam(s:fullMessage, s:"\c", s:tagColor, s:"t: \t\t", s:"\c", s:col, s:timeStr, s:"\n");
-
-      if(getCVar(SHOW_PAR_TIME_CVAR)) {
-        fullMessage = StrParam(s:fullMessage, s:"\t\t\t\t\t\t", s:"\cj", s:parStr, s:"\n");
-      }
-    }
-    if(getCVar(SHOW_LEVEL_CVAR)) {
-
-      fullMessage = StrParam(s:fullMessage, s:"\cu", n:PRINTNAME_LEVEL, s:"\n");
-      fullMessage = StrParam(s:fullMessage, s:"\cc", n:PRINTNAME_LEVELNAME, s:"\n");
+    fullMessage = StrParam(s:fullMessage, s:"\c", s:tagColor, s:"k: \t\t", s:"\c", s:countColor, d:km, s:"/", d:tm, s:"\n");
+  }
+  if(getCVar(SHOW_TIME_CVAR)) {
+    int time = Timer()/35;
+    int par = GetLevelInfo(LEVELINFO_PAR_TIME);
+    int timeTag = "t: ";
+    int timeStr = StrParam(s:timeToStr(time));
+    int parStr = StrParam(s:timeToStr(par));
+    int col = dataColor;
+    if(getCVar(COLOR_TIME_CVAR)) {
+      col = timeColor(time, par);  
     }
 
-    if(!getCVar(HIDE_TRACKER_CVAR)) {
-      HudMessage(l:fullMessage; HUDMSG_PLAIN | HUDMSG_NOTWITHFULLMAP, 400, CR_WHITE, GetCVar(X_POS_CVAR), GetCVar(Y_POS_CVAR), 1873);  
+    fullMessage = StrParam(s:fullMessage, s:"\c", s:tagColor, s:"t: \t\t", s:"\c", s:col, s:timeStr, s:"\n");
+
+    if(getCVar(SHOW_PAR_TIME_CVAR)) {
+      fullMessage = StrParam(s:fullMessage, s:"\t\t\t\t\t\t", s:"\cj", s:parStr, s:"\n");
     }
-    Delay(1);
+  }
+  if(getCVar(SHOW_LEVEL_CVAR)) {
+
+    fullMessage = StrParam(s:fullMessage, s:"\cu", n:PRINTNAME_LEVEL, s:"\n");
+    fullMessage = StrParam(s:fullMessage, s:"\cc", n:PRINTNAME_LEVELNAME, s:"\n");
+  }
+
+  if(!getCVar(HIDE_TRACKER_CVAR)) {
+    HudMessage(l:fullMessage; HUDMSG_PLAIN | HUDMSG_NOTWITHFULLMAP, 400, CR_WHITE, GetCVar(X_POS_CVAR), GetCVar(Y_POS_CVAR), 1873);  
+  }
+}
+bool running = false;
+script 400 ENTER clientside
+{
+  if(!running) {
+    running = true;
+    while(true) {
+      displayStats();
+      Delay(1);
+    }
   }
 }
